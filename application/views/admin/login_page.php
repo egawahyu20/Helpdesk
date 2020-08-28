7de8635c0cef82d0798e6bcbe168e5ddd007c778
Ega Wahyu Cahyono <45768556+egawahyu20@users.noreply.github.com>
2020-08-28 10:08:19 +0700

    tambah function login

--- application/views/admin/login_page.php
+++ application/views/admin/login_page.php
@@ -25,12 +25,15 @@
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Login Admin!</h1>
                   </div>
-                  <form class="user">
+                  <?php echo $this->session->flashdata('message'); ?> 
+                  <form class="user" method="post" action="<?php echo base_url('login')?>">
                     <div class="form-group">
-                      <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Enter Email Address...">
+                      <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Enter Email Address..." value="<?php echo set_value('email') ?>"> 
+                      <?php echo form_error('email', '<small class="text-danger pl-3">','</small>') ?>
                     </div>
                     <div class="form-group">
                       <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
+                      <?php echo form_error('password', '<small class="text-danger pl-3">','</small>') ?>
                     </div>
                     <button type="submit" class="btn btn-primary btn-user btn-block">
                       Login
@@ -38,6 +41,9 @@
                   <hr>
                   <div class="text-center">
                     <a class="small" href="forgot-password.html">Forgot Password?</a>
+                  </div>
+                  <div class="text-center">
+                    <a class="small" href="register.html">Create an Account!</a>
                   </div>
                 </div>
               </div>
