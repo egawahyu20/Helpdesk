612827ac0ee53ee343a991ad58facb5180715f72
Ega Wahyu Cahyono <45768556+egawahyu20@users.noreply.github.com>
2020-08-29 05:33:40 +0700

    Update Home.php

--- application/views/Pegawai/Home.php
+++ application/views/Pegawai/Home.php
@@ -1,7 +1,7 @@
 INI Halaman Pegawai
 
 <a href="<?php echo base_url('C_Tiket/add');?>"><button> Add Ticket NJING!!!</button></a>
-
+<a href="<?php echo base_url('Login');?>"><button> Login Admin(Teknisi)</button></a>
 <?php
 
 
